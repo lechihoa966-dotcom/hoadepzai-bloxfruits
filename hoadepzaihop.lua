ca864a920fc01270fcb91a5cdce09c35cb9ecd8e
lechihoa966-dotcom <lechihoa966@gmail.com>
2026-03-10 21:34:11 +0700

    Update hoadepzaihop.lua

--- hoadepzaihop.lua
+++ hoadepzaihop.lua
@@ -1,4 +1,6 @@
-while true do
-task.wait(3)
-game:GetService("TeleportService"):Teleport(game.PlaceId, game.Players.LocalPlayer)
+repeat task.wait() until game:IsLoaded()
+local t=game:GetService("TeleportService")
+local p=game.Players.LocalPlayer
+while task.wait(3) do
+t:Teleport(game.PlaceId,p)
 end
