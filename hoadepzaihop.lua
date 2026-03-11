2b7730736a6dd5e26fb43902af85755b323b07d9
lechihoa966-dotcom <lechihoa966@gmail.com>
2026-03-11 16:49:02 +0700

    Update hoadepzaihop.lua
    update script

--- hoadepzaihop.lua
+++ hoadepzaihop.lua
@@ -1,12 +1,14 @@
+
 repeat wait() until game:IsLoaded()
 
 local player = game.Players.LocalPlayer
 
 print("Script loaded!")
 
--- test menu
-game.StarterGui:SetCore("SendNotification",{
+game.StarterGui:SetCore("SendNotification", {
 Title="Script",
 Text="Loaded thành công!",
 Duration=5
 })
+
+print("test")
